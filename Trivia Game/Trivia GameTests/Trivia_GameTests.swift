6cbdde256af1b782093f109d6a85ec73065ddadd
mtompong <mtompong@mail.sfsu.edu>
2018-04-04 23:44:13 -0700

    Uploaded xcode project base
    project code codebase upload test

---
+++ Trivia Game/Trivia GameTests/Trivia_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Trivia_GameTests.swift
+//  Trivia GameTests
+//
+//  Created by Mark Tompong on 4/4/18.
+//  Copyright © 2018 Mark Tompong. All rights reserved.
+//
+
+import XCTest
+@testable import Trivia_Game
+
+class Trivia_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
