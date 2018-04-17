55260966fb0891789b2ddf9ef6e412cf9e6dc128
elinasuslova <elinasuslova@yahoo.com>
2018-04-17 16:09:02 -0700

    Main menu prototype

---
+++ Trivia Game/Trivia Game/CategoryTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryTableViewController.swift
+//  Trivia Game
+//
+//  Created by Elina Suslova on 4/17/18.
+//  Copyright © 2018 Mark Tompong. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+    
+    var categories: [String] = ["Category1","Category2","Category3"]
+    var selected: String?
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let category = categories[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell") as! CategoryCell
+        cell.textLabel?.text = category
+        cell.category = category
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selected = categories[indexPath.row]
+        performSegue(withIdentifier: "questionTransition", sender: self)
+    }
+    
+    /*override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ViewController {
+            destination.category = selected
+        }
+    }
+ */
+    
+}
