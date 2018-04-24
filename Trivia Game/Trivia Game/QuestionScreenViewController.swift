6e148a237b2e537fdaa4ea06743b378bc65d4570
Mark Tompong <mtompong@mail.sfsu.edu>
2018-04-23 19:33:45 -0700

    Uploaded prototype
    uploaded muliple question view controller prototype

--- Trivia Game/Trivia Game/QuestionScreenViewController.swift
+++ Trivia Game/Trivia Game/QuestionScreenViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  CNPPopupControllerE
+//
 //
 //
 
@@ -8,105 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //-----//
+    @IBOutlet weak var questionDisplay: UILabel!
+ 
+    @IBOutlet weak var answerA: UIButton!
     
-    var popupController:CNPPopupController?
-    
+    //-----//
     override func viewDidLoad() {
         super.viewDidLoad()
+        questionDisplay.text = "Question goes here"
+        answerA.setTitle("A", for: .normal)
+        
+        
+        
+        
     }
+    
+    @IBAction func submittedA(_ sender: AnyObject) {
+        performSegue(withIdentifier: "answerSubmitted", sender: sender)
+        }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
     
-    func showPopupWithStyle(_ popupStyle: CNPPopupStyle) {
-        
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineBreakMode = NSLineBreakMode.byWordWrapping
-        paragraphStyle.alignment = NSTextAlignment.center
-        
-        let title = NSAttributedString(string: "Guess the answer!", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 24), NSAttributedStringKey.paragraphStyle: paragraphStyle])
-        
-        let button = CNPPopupButton.init(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
-        button.setTitleColor(UIColor.white, for: UIControlState())
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
-        button.setTitle("Submit", for: UIControlState())
-        
-        button.backgroundColor = UIColor.blue  //(colorLiteralRed: 0.46, green: 0.8, blue: 1.0, alpha: 1.0)
-        
-        //performSegue(withIdentifier: "answerSubmitted", sender: self)
-        
-        
-        button.layer.cornerRadius = 4;
-        button.selectionHandler = { (button) -> Void in
-            self.popupController?.dismiss(animated: true)
-            print("Block for button: \(String(describing: button.titleLabel?.text))")
-        }
-        
-        let titleLabel = UILabel()
-        titleLabel.numberOfLines = 0;
-        titleLabel.attributedText = title
-        
-        let lineOneLabel = UILabel()
-        lineOneLabel.numberOfLines = 0;
-        
-        
-        let imageView = UIImageView.init(image: UIImage.init(named: "icon"))
-        
-        let lineTwoLabel = UILabel()
-        lineTwoLabel.numberOfLines = 0;
-      
-        
-        let customView = UIView.init(frame: CGRect(x: 0, y: 0, width: 250, height: 55))
-        customView.backgroundColor = UIColor.lightGray
-        
-        let textField = UITextField.init(frame: CGRect(x: 10, y: 10, width: 230, height: 35))
-        textField.borderStyle = UITextBorderStyle.roundedRect
-        textField.placeholder = "Type your answer here"
-        customView.addSubview(textField)
-        
-        let popupController = CNPPopupController(contents:[titleLabel, lineOneLabel, imageView, lineTwoLabel, customView, button])
-        popupController.theme = CNPPopupTheme.default()
-        popupController.theme.popupStyle = popupStyle
-        // LFL added settings for custom color and blur
-        popupController.theme.maskType = .custom
-        popupController.theme.customMaskColor = UIColor.clear
-        popupController.theme.blurEffectAlpha = 1.0
-        popupController.delegate = self
-        self.popupController = popupController
-        popupController.present(animated: true)
-    
-           performSegue(withIdentifier: "answerSubmitted", sender: self)
-        
-    }
-    
-    // Example action - TODO: replace with yours
-   /* @IBAction func showPopupCentered(_ sender: AnyObject) {
-        self.showPopupWithStyle(CNPPopupStyle.centered)
-    }
-     @IBAction func showPopupFormSheet(_ sender: AnyObject) {
-        self.showPopupWithStyle(CNPPopupStyle.actionSheet)
-    }
-    @IBAction func showPopupFullscreen(_ sender: AnyObject) {
-        self.showPopupWithStyle(CNPPopupStyle.fullscreen)
-    }
-    */
-    @IBAction func answerButton(_ sender: AnyObject) {
-        self.showPopupWithStyle(CNPPopupStyle.centered)
-    }
-    
+   //self.performSegue(withIdentifier: "answerSubmitted", sender: self)
     
 }
+    
 
-extension ViewController : CNPPopupControllerDelegate {
-    
-    func popupControllerWillDismiss(_ controller: CNPPopupController) {
-        print("Popup controller will be dismissed")
-    }
-    
-    func popupControllerDidPresent(_ controller: CNPPopupController) {
-        print("Popup controller presented")
-    }
-    
-}
