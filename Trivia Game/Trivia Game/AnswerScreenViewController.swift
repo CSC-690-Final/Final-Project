a4f104bd7ac837170b83e83d686cee847fb7a07d
Mark Tompong <mtompong@mail.sfsu.edu>
2018-04-24 18:20:44 -0700

    Updated Storyboard data
    QuestionviewController now passes basic data to answerViewController

--- Trivia Game/Trivia Game/AnswerScreenViewController.swift
+++ Trivia Game/Trivia Game/AnswerScreenViewController.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2018 Mark Tompong. All rights reserved.
 //
 
-import Foundation
+import UIKit
+class answerViewController: UIViewController {
+    //-----//
+    var answer:String = ""
+    
+    @IBOutlet weak var answerDisplay: UILabel?
+    
+    //-----//
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        answerDisplay?.text = answer
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
--- Trivia Game/Trivia Game/QuestionScreenViewController.swift
+++ Trivia Game/Trivia Game/QuestionScreenViewController.swift
@@ -8,34 +8,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //-----//
+    //-------------//
     @IBOutlet weak var questionDisplay: UILabel!
  
+    @IBOutlet weak var categoryDisplay: UILabel!
+    
     @IBOutlet weak var answerA: UIButton!
     
-    //-----//
+    @IBOutlet weak var answerB: UIButton!
+    
+    @IBOutlet weak var answerC: UIButton!
+    
+    @IBOutlet weak var answerD: UIButton!
+    
+    var submit = ""
+   
+    //-------------//
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        categoryDisplay.text = "Current Category"
         questionDisplay.text = "Question goes here"
         answerA.setTitle("A", for: .normal)
-        
-        
-        
-        
+        answerB.setTitle("B", for: .normal)
+        answerC.setTitle("C", for: .normal)
+        answerD.setTitle("D", for: .normal)
     }
     
-    @IBAction func submittedA(_ sender: AnyObject) {
-        performSegue(withIdentifier: "answerSubmitted", sender: sender)
-        }
+
+    @IBAction func pressedA(_ sender: Any) {
+        self.submit = self.answerA.currentTitle!
+        performSegue(withIdentifier: "submittedAnswer", sender: self)
+    }
+ 
+    @IBAction func pressedB(_ sender: Any) {
+        self.submit = self.answerB.currentTitle!
+        performSegue(withIdentifier: "submittedAnswer", sender: self)
+    }
+    
+    @IBAction func pressedC(_ sender: Any) {
+        self.submit = self.answerC.currentTitle!
+        performSegue(withIdentifier: "submittedAnswer", sender: self)
+    }
+    
+    @IBAction func pressedD(_ sender: Any) {
+        self.submit = self.answerD.currentTitle!
+        performSegue(withIdentifier: "submittedAnswer", sender: self)
+    }
     
     
+    
+    //-------------//
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let vc = segue.destination as! answerViewController
+        vc.answer = self.submit
+        
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-  
-    
-   //self.performSegue(withIdentifier: "answerSubmitted", sender: self)
     
 }
     
