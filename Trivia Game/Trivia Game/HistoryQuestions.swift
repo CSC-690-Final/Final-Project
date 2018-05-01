9cd3ff971d020763a62d7be807305da5cb26b4a7
elinasuslova <elinasuslova@yahoo.com>
2018-04-30 23:37:27 -0700

    History questions file, literature questions file

---
+++ Trivia Game/Trivia Game/HistoryQuestions.swift
@@ -0,0 +1,25 @@
+//
+//  HistoryQuestions.swift
+//  Trivia Game
+//
+//  Created by Elina Suslova on 4/30/18.
+//  Copyright © 2018 Mark Tompong. All rights reserved.
+//
+
+import Foundation
+class HistoryQuestions{
+    var list = [Question]()
+    
+    init(){
+        list.append(Question(questionText: "Which country did Germany invade on the 1st of September 1939?", choiceA: "France", choiceB: "Czechoslovakia", choiceC: "Poland", choiceD: "Finland", answer: 3))
+        list.append(Question(questionText: "What is the oldest known story in the world?" , choiceA: "The Bible", choiceB: "The Histories", choiceC: "The Epic of Gilgamesh", choiceD: "The Odyssey", answer: 3))
+        list.append(Question(questionText: "Which of these was one of the seven ancient wonders of the world?", choiceA: "Great Wall of China", choiceB: "Macchu Picchu", choiceC: "Lighthouse of Alexandria", choiceD: "Taj Mahal", answer: 3))
+        list.append(Question(questionText: "The late 18th and early 19th centuries are often referred as the Industrial Revolution. In which industry did this era of modernization start on a large and ongoing scale?", choiceA: "Steel production", choiceB: "Glass making", choiceC: "Shipbuilding", choiceD: "Textiles", answer: 4))
+        list.append(Question(questionText: "In AD 476 the last Western emperor, Romulus Augutulus was overthrown by Odoacer, a barbarian. This is considered by some to be the possible start of which European era?", choiceA: "The Inquisition", choiceB: "The High Middles Ages", choiceC: "The Ming Dynasty", choiceD: "The Dark Ages", answer: 4))
+        list.append(Question(questionText: "The Meiji Period was a period of enlightenment and technological growth that occurred from 1868 to 1912 which brought which east Asian country to the status of a world power?", choiceA: "Japan", choiceB: "China", choiceC: "Cambodia", choiceD: "Pakistan", answer: 1))
+        list.append(Question(questionText: "Which country's ancient history includes the times of 'The Old Kingdom', 'The Middle Kingdom' and 'The New Kingdom'?", choiceA: "China", choiceB: "Mexico", choiceC: "France", choiceD: "Egypt", answer: 4))
+        list.append(Question(questionText: "Which enlightened period of Chinese history is noted for, among other things, the fabulous vases that were produced and are often on display at museums?", choiceA: "The Great Leap Forward", choiceB: "The Porcelain Era", choiceC: "The Ming Dynasty", choiceD: "The Great Blossoming", answer: 3))
+        list.append(Question(questionText: "What do we call the period between the 14th and 17th centuries in Europe that was considered a rebirth?", choiceA: "The Industrial Revolution", choiceB: "The Flapper Age", choiceC: "The Inquisition", choiceD: "The Renaissance", answer: 4))
+        list.append(Question(questionText: "‘The Jacobean Era’ refers to a period of time in the early 17th century in which of the following countries?", choiceA: "Jordan", choiceB: "England", choiceC: "Malaysia", choiceD: "Tunisia", answer: 2))
+    }
+}
