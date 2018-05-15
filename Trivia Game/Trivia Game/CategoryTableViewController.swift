9f10a715d508ffd514b2eea839e416119ae6e7f8
elinasuslova <elinasuslova@yahoo.com>
2018-05-14 20:48:10 -0700

    main menu screen visual representation updates

--- Trivia Game/Trivia Game/CategoryTableViewController.swift
+++ Trivia Game/Trivia Game/CategoryTableViewController.swift
@@ -9,21 +9,31 @@
 import UIKit
 
 class CategoryTableViewController: UITableViewController {
+   
+    let cellSpacingHeight: CGFloat = 10
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        //let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+       
+       // let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         //backgroundImage.image = UIImage(named: "trivia_background.jpg")
-        //backgroundImage.contentMode = UIViewContentMode.scaleAspectFill
+       // backgroundImage.contentMode = UIViewContentMode.scaleAspectFill
         //self.view.insertSubview(backgroundImage, at: 0)
-        
+        tableView.backgroundView = UIImageView(image: UIImage(named: "trivia_background.jpg"))
     }
     var categories: [String] = ["General Knowledge","History","Movies","Literature","Astronomy"]
     var selected: String?
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
+    }
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return cellSpacingHeight
+    }
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
     }
     
     
@@ -34,6 +44,13 @@ class CategoryTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell") as! CategoryCell
         cell.textLabel?.text = category
         cell.category = category
+        cell.textLabel?.font = UIFont(name: "Avenir", size:22);
+        cell.backgroundColor = UIColor(white: 1, alpha: 0.75)
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 3
+        cell.layer.cornerRadius = 20
+        cell.clipsToBounds = true
+
         return cell
     }
     
